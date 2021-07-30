6cecc9965c2fc47e1b2302a9f8eb647ece390dfb
yuki-1107 <jubeatknit18@gmail.com>
2021-07-30 12:26:56 +0000

    devise うまくできない

--- app/models/order_dateil.rb
+++
@@ -1,2 +0,0 @@
-class OrderDateil < ApplicationRecord
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :models
   devise_for :admins
   devise_for :costomers
